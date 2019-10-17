42adb25cb50c00c34de466fdf18ea86c50695647
youngwoo.cho <petshop0907@gmail.com>
2019-10-16 20:06:34 -0400

    basic App.js with render function that renders navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/navbar";
+import Wrapper from "./components/wrapper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    score: 0,
+    highscore: 0,
+    // clickCheck
+    // navMsg
+    navMsg: "Click an image to start the game!",
+    // navMsgColor
+    navMsgColor: ''
+    // shakeEffect
+    // 
+
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          score={this.state.score}
+          highscore={this.state.highscore}
+          navMsg={this.state.navMsg}
+          navMsgColor={this.state.navMsgColor}
+        />
+      </Wrapper>
+    )
+  }
 }
 
 export default App;
