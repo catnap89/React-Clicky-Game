e9aae5de7a6e38d7ff7d0ba1b4fd1ee7b185eb16
youngwoo.cho <petshop0907@gmail.com>
2019-10-18 13:02:26 -0400

    container components

---
+++ src/components/container/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Character from "../character";  // Q: Is it okay to refer the directory instead of the actual file?
+
+const Container = props => (
+  <div className={
+    props.shake? 
+    "container d-flex flex-wrap justify-content-center shake" : "container d-flex flex-wrap justify-content-center"
+  }>
+    {/* props.character is given value in App.js render function */}
+    {props.characters.map((a, i) => <Character name={a} key={i} clickEvent={props.clickEvent} />)}
+  </div>
+)
+
+export default Container;
