9459a27299932e701547a5517b6173babf0e6a17
youngwoo.cho <petshop0907@gmail.com>
2019-10-18 18:20:22 -0400

    Updates

---
+++ precache-manifest.5532dc0f891aa87b5ad872f38a0ec18c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc483c4408ce107a7cc6969aa2fd041",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "bb3d54dedb9de37ef8a2",
+    "url": "/React-Clicky-Game/static/css/main.8f8d3e23.chunk.css"
+  },
+  {
+    "revision": "0d5f2c231943a09cca19",
+    "url": "/React-Clicky-Game/static/js/2.646fb832.chunk.js"
+  },
+  {
+    "revision": "bb3d54dedb9de37ef8a2",
+    "url": "/React-Clicky-Game/static/js/main.d1337c81.chunk.js"
+  },
+  {
+    "revision": "aa73c1f9465f49c7f12e",
+    "url": "/React-Clicky-Game/static/js/runtime-main.e378ad77.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/React-Clicky-Game/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/React-Clicky-Game/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/React-Clicky-Game/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/React-Clicky-Game/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/React-Clicky-Game/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/React-Clicky-Game/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/React-Clicky-Game/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/React-Clicky-Game/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/React-Clicky-Game/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/React-Clicky-Game/static/media/mr.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/React-Clicky-Game/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/React-Clicky-Game/static/media/summer.afdbb7bf.png"
+  }
+]);
