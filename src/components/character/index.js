06c1081e4e7962166ae0f3dd0de62b1d6c1973bc
youngwoo.cho <petshop0907@gmail.com>
2019-10-18 17:56:53 -0400

    changed name of the props from clickEvent to clickEvt

--- src/components/character/index.js
+++ src/components/character/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 // the onClick function is defined as a callback so that the clicked elements value
 // can be passed to props.clickEvent to check if the image has been clicked or not
 const Character = props => (
-  <div className="card" onClick={e => props.clickEvent(e.target.src)}>
+  <div className="card" onClick={e => props.clickEvt(e.target.src)}>
     <img className="card-img-top card-height" src={props.name} alt="" />
   </div>
 );
