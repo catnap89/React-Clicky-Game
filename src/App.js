299bcaa84ca45ab5f62d7fc31ae28576711f5daf
youngwoo.cho <petshop0907@gmail.com>
2019-10-18 13:03:35 -0400

    App.js with render function and components imported

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar";
 import Wrapper from "./components/wrapper";
+import Jumbotron from "./components/jumbotron";
+import Container from "./components/container";
+import Footer from "./components/footer";
+import images from "./assets/javascript/images";
 
 class App extends Component {
 
   state = {
     score: 0,
     highscore: 0,
-    // clickCheck
-    // navMsg
     navMsg: "Click an image to start the game!",
-    // navMsgColor
-    navMsgColor: ''
-    // shakeEffect
-    // 
+    navMsgColor: '',
+    allCharacters: this.randomArray(),
+    clickedCharacter: [],
+    shake: false
+  }
+
+  randomArray() {
+    // make new copy of images array
+    const newCharacterArray = images.slice();
+    // stores random (shuffled) image array
+    const randomArr = [];
 
   }
 
@@ -26,6 +35,13 @@ class App extends Component {
           navMsg={this.state.navMsg}
           navMsgColor={this.state.navMsgColor}
         />
+        <Jumbotron />
+        <Container 
+          shake={this.state.shake}
+          characters={this.state.allCharacters}
+          clickEvt={this.state.clickEvt}
+        />
+        <Footer />
       </Wrapper>
     )
   }
