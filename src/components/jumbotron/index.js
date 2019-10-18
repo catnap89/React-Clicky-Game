d473956b69ea3fc95623c873fc2686fededc6042
youngwoo.cho <petshop0907@gmail.com>
2019-10-18 13:02:44 -0400

    jumbotron components

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Jumbotron = props => (
+  <div className="jumbotron text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
+  </div>
+);
+
+export default Jumbotron
